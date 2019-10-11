a246546a0a5fc66dd672297e2c0217f71a1baf7e
Your Name <you@example.com>
2019-10-11 13:59:02 +0200

    cards changed

--- resources/views/components/header-cards.blade.php
+++ resources/views/components/header-cards.blade.php
@@ -7,11 +7,11 @@
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Recruiters</div>
+                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Messages</div>
                         <div class="h5 mb-0 font-weight-bold text-gray-800">2</div>
                     </div>
                     <div class="col-auto">
-                        <i class="fas fa-users fa-2x text-gray-300"></i>
+                        <i class="fas fa-envlope fa-2x text-gray-300"></i>
                     </div>
                 </div>
             </div>
@@ -24,7 +24,7 @@
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1"> Companies </div>
+                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1"> Appointments </div>
                         <div class="h5 mb-0 font-weight-bold text-gray-800">4</div>
                     </div>
                     <div class="col-auto">
@@ -41,7 +41,7 @@
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1"> Job Seekers </div>
+                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1"> Connections </div>
                         <div class="h5 mb-0 font-weight-bold text-gray-800">4</div>
                     </div>
                     <div class="col-auto">
